37ab3955099d2851db80e26ccabd0c9310a80056
joseungw <101791486+joseungw@users.noreply.github.com>
2022-03-22 15:34:45 +0900

    Add files via upload

--- Day0322/src/Gugudan.java
+++ Day0322/src/Gugudan.java
@@ -3,8 +3,8 @@ public class Gugudan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int g=1, x=3,p=0;
-		System.out.println("구구단 3단 출력");
+		int g=1, x=2,p=0;
+		System.out.println("구구단 2단 출력");
 		for (g=1;g<10;g++) {
 			p=g*x;
 			System.out.println(p);
---
+++ Day0322/src/Gugudan3.java
@@ -0,0 +1,18 @@
+
+public class Gugudan3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("구구단 2-9단 출력");
+		int a = 0;
+		for (int i=2;i<10;i++) 
+		{
+			for (a =1;a<10;a++) 
+			{
+				System.out.print(a+"*"+i+"="+a*i+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
